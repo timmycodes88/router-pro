80c372bd80eb5b3b262279aab03e67534d77f670
Tim <tim@brightsidedeveloper.com>
2023-02-06 20:53:43 -0600

    JSDocs

--- src/api/StatsAPI.js
+++ src/api/StatsAPI.js
@@ -1,8 +1,22 @@
 import { get } from "../utils/request"
 
+/**
+ * @typedef {Object} StatsResponse
+ * @property {number} acellusID
+ * @property {number} gold
+ * @property {number} silver
+ * @property {number} bronze
+ * @property {number} gpa
+ */
+
 const Endpoint = "stats"
 
 const StatsAPI = {
+  /**
+   *
+   * @param {string} acellusID
+   * @returns {StatsResponse} StatsResponse
+   */
   get: acellusID => get(Endpoint, { acellusID }),
 }
 
